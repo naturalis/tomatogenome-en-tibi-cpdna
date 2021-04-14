433ce120a6e04596743e97f057af435fec5bb142
Vasia Kakakiou <77166545+vasia07@users.noreply.github.com>
2021-04-15 01:01:30 +0300

    Update blast_script.py

--- script/blast_script.py
+++ script/blast_script.py
@@ -8,15 +8,15 @@ for record in sequences:             #for each record in sequences
     print(repr(record.seq))          #print representation
     print(len(record))               #print length
     result_handle = NCBIWWW.qblast("blastn", "nt", record.seq)  #run blastn
-  with open("my_blast.xml", "a") as blast_file: # create and then append an xml output file
+    with open("my_blast.xml", "a") as blast_file: # create and then append an xml output file
        blast_file.write(result_handle.read())
 
-       result_handle.close() #tidy up
+    result_handle.close() #tidy up
 
 from Bio.Blast import NCBIXML
 
 blast_records = NCBIXML.parse(result_handle) #parse xml
-   for blast_record in blast_records: #for each blast_record
-       print("BLAST result for sequence:", blast_record.query)
-       print("Number of alignments:", len(blast_record.alignments))
-       print()
+for blast_record in blast_records: #for each blast_record
+    print("BLAST result for sequence:", blast_record.query)
+    print("Number of alignments:", len(blast_record.alignments))
+    print()
