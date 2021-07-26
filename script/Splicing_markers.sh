fe9256f4285562b6eb4b67c1da40ac5e8c140304
Vasia Kakakiou <77166545+vasia07@users.noreply.github.com>
2021-07-26 15:13:13 +0300

    Add files via upload

---
+++ script/Splicing_markers.sh
@@ -0,0 +1,11 @@
+## Bash Script to loop samtoolsfaidx
+## Input files have .fasta extension
+
+fastafiles=$(ls *.fasta)
+
+for fasta in $fastafiles; do
+idseq=$(basename $fasta .pileup.fasta)
+extension=".markdup.bam"
+result="$idseq$extension"
+samtools faidx $fasta $result:from_to_coordinates -o ${idseq}.marker_name.fa
+done
