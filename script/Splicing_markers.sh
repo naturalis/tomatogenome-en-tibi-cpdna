a198db69cd7aaae9fdc032003d1490c7b56ad74e
Vasia Kakakiou <77166545+vasia07@users.noreply.github.com>
2021-08-08 14:56:18 +0300

    Update Splicing_markers.sh

--- script/Splicing_markers.sh
+++ script/Splicing_markers.sh
@@ -7,5 +7,5 @@ for fasta in $fastafiles; do
 idseq=$(basename $fasta .pileup.fasta)
 extension=".markdup.bam"
 result="$idseq$extension"
-samtools faidx $fasta $result:from_to_coordinates -o ${idseq}.marker_name.fa
+samtools faidx $fasta $result:*from_to_coordinates* -o ${idseq}.marker_name.fa
 done
