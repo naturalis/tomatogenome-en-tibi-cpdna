31b55f7dcc6ae87274ab10f1947242dc97f99627
Vasia Kakakiou <77166545+vasia07@users.noreply.github.com>
2021-08-08 15:04:03 +0300

    Update Concatenated_matrix.sh

--- script/Concatenated_matrix.sh
+++ script/Concatenated_matrix.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-#SBATCH --ntasks=1
-#SBATCH --partition=debug
-#SBATCH --output=output_%j.txt
-#SBATCH --error=error_output_%j.txt
-#SBATCH --job-name=concatenated_matrix
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=vkakakiou@gmail.com
-
 phyutility -concat -in ndhC-trnV_aligned.fasta ndhF_aligned.fasta petA-psbJ_aligned.fasta psbE-petL_aligned.fasta rbcL-accD_aligned.fasta rpl32-trnL_aligned.fasta rpoB-trnC_aligned.fasta rps16-trnQ_aligned.fasta trnH-psbA_aligned.fasta trnK_aligned.fasta trnS-trnG_aligned.fasta ycf1_aligned.fasta -out concatenated_matrix.nex
