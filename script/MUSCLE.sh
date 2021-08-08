db39819486dd34677db3d6600276d2779b0f80b4
Vasia Kakakiou <77166545+vasia07@users.noreply.github.com>
2021-08-08 15:02:36 +0300

    Update MUSCLE.sh

--- script/MUSCLE.sh
+++ script/MUSCLE.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#SBATCH --ntasks=1
-#SBATCH --partition=debug
-#SBATCH --output=output_%j.txt
-#SBATCH --error=error_output_%j.txt
-#SBATCH --job-name=MUSCLE
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=vkakakiou@gmail.com
-
 cd /home/vasia.kakakiou/samtools/bin/FASTA/Extracted_markers
 
 ## Bash Script to loop MUSCLE
